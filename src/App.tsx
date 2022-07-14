dd8bc159906a9cda2558a1c596b73ddc08f26903
LoganW <logan.willaumez@gmail.com>
2022-07-14 18:20:13 +0200

    [FEAT](randomuser): add good type for data fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
 import { useEffect, useState } from 'react';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import './App.scss';
 import Header from './Header/Header';
+import { fetchRandomUser, randomUser } from '../interfaces/fetchRandomUser';
 
-const baseURL = 'https://randomuser.me/api/';
+const baseURL = 'https://randomuser.me/api/?results=3';
 
 function App() {
-  const [dataUser, setDataUser] = useState([]);
+  const [dataUser, setDataUser] = useState<randomUser[]>([]);
   const [isClick, setIsClick] = useState<boolean>(false);
   useEffect(() => {
-    axios.get(baseURL).then((response) => {
-      setDataUser(response.data.results[0]);
+    axios.get<fetchRandomUser[]>(baseURL).then((response: AxiosResponse) => {
+      setDataUser(response.data.results);
     });
   }, []);
-  return (
-    <div className='App'>
-      <Header dataUser={dataUser} />
-    </div>
-  );
+  return <div className='App'>{<Header dataUser={dataUser} />}</div>;
 }
 
 export default App;
--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,29 +1,30 @@
 import './Header.scss';
+import { randomUser } from '../../interfaces/fetchRandomUser';
 
-function Header({ dataUser }) {
+function Header({ dataUser }: { dataUser: randomUser[] }) {
   return (
     <div className='sidebar'>
       <div className='profil'>
-        <h2 className='profil__firstName'>{dataUser?.name.first}</h2>
+        <h2 className='profil__firstName'>{dataUser[0]?.name.first}</h2>
         <div className='profil__location'>
           <i className='fa-solid fa-location-pin' />
-          <p className='profil__city'>{dataUser?.location.country}</p>
+          <p className='profil__city'>{dataUser[0]?.location.country}</p>
         </div>
         <div className='profil__picture'>
           <a
             className='profil__icons profil__icons--email'
-            href={`mailto:${dataUser?.email}`}
+            href={`mailto:${dataUser[0]?.email}`}
           >
             <i className='fa-solid fa-envelope' />
           </a>
           <img
             className='profil__img'
-            src={dataUser?.picture.medium}
-            alt={`picture of ${dataUser?.name.first}`}
+            src={dataUser[0]?.picture.medium}
+            alt={`picture of ${dataUser[0]?.name.first}`}
           />
           <a
             className='profil__icons profil__icons--phone'
-            href={`tel:${dataUser?.phone}`}
+            href={`tel:${dataUser[0]?.phone}`}
           >
             <i className='fa-solid fa-phone' />
           </a>
