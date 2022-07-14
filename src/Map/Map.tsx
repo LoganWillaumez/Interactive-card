1a2c1bfa245d8550e59c8a45c8ce8a5f88327ab4
LoganW <logan.willaumez@gmail.com>
2022-07-14 23:40:48 +0200

    [FEAT](map): add google map api

--- src/Map/Map.tsx
+++ src/Map/Map.tsx
@@ -1,5 +1,21 @@
 import './Map.scss';
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 
 export const Map = () => {
-  return <div>Map</div>;
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: import.meta.env.VITE_KEY,
+  });
+  return (
+    <div className='map'>
+      {isLoaded ? (
+        <GoogleMap
+          zoom={10}
+          center={{ lat: 44, lng: -80 }}
+          mapContainerClassName='map-container'
+        />
+      ) : (
+        <div>no</div>
+      )}
+    </div>
+  );
 };
