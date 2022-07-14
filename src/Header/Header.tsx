1b30d001d0eedb583505daa8cd0edd50e36ebdba
LoganW <logan.willaumez@gmail.com>
2022-07-14 19:02:43 +0200

    [MERGE](header-responsive): add responsive of header component

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -7,27 +7,29 @@ function Header({ dataUser }: { dataUser: randomUser[] }) {
       <div className='profil'>
         <h2 className='profil__firstName'>{dataUser[0]?.name.first}</h2>
         <div className='profil__location'>
-          <i className='fa-solid fa-location-pin' />
+          <i className='profil__point fa-solid fa-location-pin' />
           <p className='profil__city'>{dataUser[0]?.location.country}</p>
         </div>
         <div className='profil__picture'>
-          <a
-            className='profil__icons profil__icons--email'
-            href={`mailto:${dataUser[0]?.email}`}
-          >
-            <i className='fa-solid fa-envelope' />
-          </a>
           <img
             className='profil__img'
-            src={dataUser[0]?.picture.medium}
+            src={dataUser[0]?.picture.large}
             alt={`picture of ${dataUser[0]?.name.first}`}
           />
-          <a
-            className='profil__icons profil__icons--phone'
-            href={`tel:${dataUser[0]?.phone}`}
-          >
-            <i className='fa-solid fa-phone' />
-          </a>
+          <div className='profil__social'>
+            <a
+              className='profil__icons profil__icons--email'
+              href={`mailto:${dataUser[0]?.email}`}
+            >
+              <i className='fa-solid fa-envelope' />
+            </a>
+            <a
+              className='profil__icons profil__icons--phone'
+              href={`tel:${dataUser[0]?.phone}`}
+            >
+              <i className='fa-solid fa-phone' />
+            </a>
+          </div>
         </div>
       </div>
     </div>
