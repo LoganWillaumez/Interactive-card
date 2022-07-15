e30fbbc9fca9eda255ee3532391f06acb9c97660
LoganW <logan.willaumez@gmail.com>
2022-07-15 09:23:49 +0200

    [MERGE](contact): add form contact

--- src/Contact/Contact.tsx
+++ src/Contact/Contact.tsx
@@ -1,5 +1,82 @@
+import { useState } from 'react';
+import { Input } from '../Input/Input';
 import './Contact.scss';
 
 export const Contact = () => {
-  return <div>Contact</div>;
+  const [value, setValue] = useState({
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+  });
+  const [modale, setModale] = useState<boolean>(false);
+  const handleChange = (e: any) => {
+    setValue({ ...value, [e.target.name]: e.target.value });
+  };
+  const seeModale = () => {
+    setModale(true);
+    setTimeout(() => setModale(false), 3000);
+  };
+  return (
+    <div className='contact'>
+      <h4 className='contact__title'>Please, contact me!</h4>
+      <form
+        className='contact__form'
+        action=''
+        onSubmit={(e) => {
+          e.preventDefault();
+          setValue({
+            name: '',
+            email: '',
+            subject: '',
+            message: '',
+          });
+          seeModale();
+        }}
+      >
+        <Input
+          placeholder='Your name*'
+          name='name'
+          required={true}
+          value={value.name}
+          type={'text'}
+          pattern={'[a-zA-Z]{3,30}'}
+          handleChange={handleChange}
+        />
+        <Input
+          placeholder='Your email*'
+          type='email'
+          name='email'
+          value={value.email}
+          handleChange={handleChange}
+          required={true}
+          pattern='[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$'
+        />
+        <Input
+          placeholder='Your subject*'
+          type={'text'}
+          value={value.subject}
+          handleChange={handleChange}
+          name='subject'
+          required={true}
+          pattern={'[a-zA-Z]{3,30}'}
+        />
+        <Input
+          placeholder='Your message*'
+          name='message'
+          value={value.message}
+          type={'textarea'}
+          handleChange={handleChange}
+          required={true}
+          pattern={'[a-zA-Z]{3,150}'}
+        />
+        <button type='submit' className='button-contact button--disable'>
+          Send
+        </button>
+      </form>
+      <div className={`modale ${modale ? 'modale--see' : ''}`}>
+        Message send !
+      </div>
+    </div>
+  );
 };
---
+++ src/Input/Input.tsx
@@ -0,0 +1,31 @@
+import './Input.scss';
+export const Input = ({
+  pattern,
+  value,
+  type,
+  name,
+  placeholder,
+  required,
+  handleChange,
+}: {
+  pattern: string;
+  name: string;
+  value: string;
+  type: string;
+  placeholder: string;
+  required: boolean;
+  handleChange: Function;
+}) => {
+  return (
+    <input
+      className='input'
+      type={type}
+      pattern={pattern}
+      value={value}
+      placeholder={placeholder}
+      required={required}
+      name={name}
+      onChange={(e) => handleChange(e)}
+    />
+  );
+};
