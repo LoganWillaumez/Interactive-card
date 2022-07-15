8e908e1fb4b618d5d2f8947a0096813c1da7c6f9
LoganW <logan.willaumez@gmail.com>
2022-07-15 11:45:45 +0200

    [FIX](fix-bug): fix problem on design and scroll of header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -16,83 +16,85 @@ function Header({ dataUser }: { dataUser: randomUser[] }) {
   };
   return (
     <div className='sidebar'>
-      <div className='profil'>
-        <h2 className='profil__firstName'>{`${dataUser[0]?.name.first}, ${dataUser[0]?.dob.age}`}</h2>
-        <div className='profil__location'>
-          <i className='profil__point fa-solid fa-location-pin' />
-          <p className='profil__city'>{`${dataUser[0]?.location.city}, ${dataUser[0]?.location.country}`}</p>
+      <div className='sidebar__scroll'>
+        <div className='profil'>
+          <h2 className='profil__firstName'>{`${dataUser[0]?.name.first}, ${dataUser[0]?.dob.age}`}</h2>
+          <div className='profil__location'>
+            <i className='profil__point fa-solid fa-location-pin' />
+            <p className='profil__city'>{`${dataUser[0]?.location.city}, ${dataUser[0]?.location.country}`}</p>
+          </div>
+          <div className='profil__picture'>
+            <img
+              className='profil__img'
+              src={dataUser[0]?.picture.large}
+              alt={`picture of ${dataUser[0]?.name.first}`}
+            />
+          </div>
+          <div className='profil__friends'>
+            <button
+              type='button'
+              className='button--disable'
+              onClick={() => toggleFriends()}
+            >
+              <i className='fa-solid fa-user-group'></i>
+            </button>
+            <MenuFriends
+              dataUser={dataUser}
+              friendsOpen={friendsOpen}
+              toggleFriends={toggleFriends}
+            />
+          </div>
+          <div className='profil__burger'>
+            <button
+              type='button'
+              className='button--disable button__burger'
+              onClick={() => toggleBurger()}
+            >
+              <i className='fa-solid fa-bars'></i>
+            </button>
+            <BurgerMenu burgerOpen={burgerOpen} toggleBurger={toggleBurger} />
+          </div>
         </div>
-        <div className='profil__picture'>
-          <img
-            className='profil__img'
-            src={dataUser[0]?.picture.large}
-            alt={`picture of ${dataUser[0]?.name.first}`}
-          />
+        <div className='friends'>
+          <h4 className='friends__title'>Friends :</h4>
+          <div className='friends__data'>
+            {dataUser?.map((data, i) => {
+              return (
+                i !== 0 && (
+                  <div
+                    key={dataUser[0]?.login.uuid + i}
+                    className='friends__global'
+                  >
+                    <img
+                      className='friends__img'
+                      src={data.picture.large}
+                      alt=''
+                    />
+                    <p className='friends__name'>{data.name.first}</p>
+                  </div>
+                )
+              );
+            })}
+          </div>
         </div>
-        <div className='profil__friends'>
-          <button
-            type='button'
-            className='button--disable'
-            onClick={() => toggleFriends()}
+        <div className='links'>
+          <NavLink
+            to='/'
+            className={({ isActive }) =>
+              isActive ? 'nav-link nav-link--selected' : 'nav-link'
+            }
           >
-            <i className='fa-solid fa-user-group'></i>
-          </button>
-          <MenuFriends
-            dataUser={dataUser}
-            friendsOpen={friendsOpen}
-            toggleFriends={toggleFriends}
-          />
-        </div>
-        <div className='profil__burger'>
-          <button
-            type='button'
-            className='button--disable button__burger'
-            onClick={() => toggleBurger()}
+            Map
+          </NavLink>
+          <NavLink
+            to='/map'
+            className={({ isActive }) =>
+              isActive ? 'nav-link nav-link--selected' : 'nav-link'
+            }
           >
-            <i className='fa-solid fa-bars'></i>
-          </button>
-          <BurgerMenu burgerOpen={burgerOpen} toggleBurger={toggleBurger} />
+            Contact
+          </NavLink>
         </div>
-      </div>
-      <div className='friends'>
-        <h4 className='friends__title'>Friends :</h4>
-        <div className='friends__data'>
-          {dataUser?.map((data, i) => {
-            return (
-              i !== 0 && (
-                <div
-                  key={dataUser[0]?.login.uuid + i}
-                  className='friends__global'
-                >
-                  <img
-                    className='friends__img'
-                    src={data.picture.large}
-                    alt=''
-                  />
-                  <p className='friends__name'>{data.name.first}</p>
-                </div>
-              )
-            );
-          })}
-        </div>
-      </div>
-      <div className='links'>
-        <NavLink
-          to='/'
-          className={({ isActive }) =>
-            isActive ? 'nav-link nav-link--selected' : 'nav-link'
-          }
-        >
-          Map
-        </NavLink>
-        <NavLink
-          to='/map'
-          className={({ isActive }) =>
-            isActive ? 'nav-link nav-link--selected' : 'nav-link'
-          }
-        >
-          Contact
-        </NavLink>
       </div>
     </div>
   );
