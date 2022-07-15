82b624b12cd2a2f0f240d19d934ffadf0be9b61b
LoganW <logan.willaumez@gmail.com>
2022-07-15 14:09:31 +0200

    [FEAT](semantic-html): add alt of img

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -17,7 +17,7 @@ function Header({ dataUser }: { dataUser: randomUser[] }) {
   return (
     <div className='sidebar'>
       <div className='sidebar__scroll'>
-        <div className='profil'>
+        <section className='profil'>
           <h2 className='profil__firstName'>{`${dataUser[0]?.name.first}, ${dataUser[0]?.dob.age}`}</h2>
           <div className='profil__location'>
             <i className='profil__point fa-solid fa-location-pin' />
@@ -54,7 +54,7 @@ function Header({ dataUser }: { dataUser: randomUser[] }) {
             </button>
             <BurgerMenu burgerOpen={burgerOpen} toggleBurger={toggleBurger} />
           </div>
-        </div>
+        </section>
         <div className='friends'>
           <h4 className='friends__title'>Friends :</h4>
           <div className='friends__data'>
@@ -68,7 +68,7 @@ function Header({ dataUser }: { dataUser: randomUser[] }) {
                     <img
                       className='friends__img'
                       src={data.picture.large}
-                      alt=''
+                      alt={`picture of ${data?.name.first}`}
                     />
                     <p className='friends__name'>{data.name.first}</p>
                   </div>
